93e0d4998b9814456c8040fddea7eb141ab3719d
BOSS2356 <opr7454188@outlook.com>
2024-08-27 12:38:41 +0800

    Site updated: 2024-08-27 12:38:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/24/hello-world/"];function toRandomPost(){
+var posts=["2024/08/24/hello-world/","2024/08/27/你好/","2024/08/27/你好，我是 Iriving/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
